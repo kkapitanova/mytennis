fb0b3e551eb12a81071ef1568b33ff9237f2998a
k.kapitanova <k.kapitanova@infoleven.com>
2022-05-01 13:45:29 +0300

    quick fix remove logout success component

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,5 @@
 export { default as Login } from './login/Login';
 export { default as Register } from './register/Register';
-export { default as LogoutSuccess } from './logout-success/LogoutSuccess';
 export { default as Profile } from './profile/Profile';
 export { default as Home } from './home/Home'
 export { default as Players } from './players/Players';
@@ -10,10 +9,3 @@ export { default as TournamentSubmission } from './tournament-submission/Tournam
 export { default as MyTournaments } from './my-tournaments/MyTournaments';
 export { default as Chats } from './chats/Chats';
 export { default as About } from './about/About';
-
-// export { default as Home } from './home/Home'
-// export { default as About } from './about/About'
-// export { default as Contacts } from './contacts/Contacts'
-// export { default as FAQ } from './faq/FAQ'
-// export { default as PageNotFound } from './page-not-found/PageNotFound'
-// export { default as LoadingScreen } from './loading-screen/LoadingScreen'
