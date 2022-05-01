1af6b12e654c5c9730b94f4aaf736c0eb17fcd29
k.kapitanova <k.kapitanova@infoleven.com>
2022-05-01 13:37:18 +0300

    move footer and navbar to components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
-export { default as Table } from './table/Table.jsx'
+export { default as Table } from './table/Table.jsx';
+export { default as NavBar } from './navbar/NavBar.jsx';
+export { default as Footer } from './footer/Footer.jsx';
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,3 @@
-export { default as NavBar } from './navbar/NavBar';
 export { default as Login } from './login/Login';
 export { default as Register } from './register/Register';
 export { default as LogoutSuccess } from './logout-success/LogoutSuccess';
@@ -11,7 +10,6 @@ export { default as TournamentSubmission } from './tournament-submission/Tournam
 export { default as MyTournaments } from './my-tournaments/MyTournaments';
 export { default as Chats } from './chats/Chats';
 export { default as About } from './about/About';
-export { default as Footer } from './footer/Footer';
 
 // export { default as Home } from './home/Home'
 // export { default as About } from './about/About'
